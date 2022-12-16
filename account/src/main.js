243270692123f4e6e24dc77b53fc343e4598910a
Samara Flor <samaraflor2013>
2022-12-16 09:20:49 -0300

    entrega sprint-5

--- account/src/main.js
+++ account/src/main.js
@@ -1,14 +1,11 @@
-import express, { json } from 'express';
-import router from "./routes/routes.js"
+import express  from "express";
+import { router } from "./routes.js";
+import {router} from "./routes/routes.js"
 
-const port = 3000;
 const app = express();
-
-app.use(json());
+app.use(express.json());
 app.use(router);
 
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, function(){
+    console.log('accounts server is running');  
 });
