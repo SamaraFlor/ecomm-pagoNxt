c5401d292a6b1e322e16945c9aa4c533a825c5e6
Samara Flor <samaraflor2013>
2023-01-24 17:51:14 -0300

    Autorizando o usuário

--- product/src/routes.js
+++ product/src/routes.js
@@ -1,13 +1,32 @@
 import { Router } from 'express';
+import { decriptToken } from './helpers/token.js';
 import { createProductUseCase } from './use-case/createProductUseCase.js';
-import {listProductsUseCase} from './use-case/listProducts.js'
-
+import {listProductsUseCase} from "./use-case/listProducts.js"
 
 const router = Router();
 
 router.post('/products', async (request, response) => {
+    const authorizationHeader = request.headers.authorization;
+    if(!authorizationHeader) {
+        return response.status(401).json({ message: 'authentication required' })
+    }
+    
+    const token = authorizationHeader.split(' ')[1];
+    
+    if(!token) {
+        return response.status(400).json({ message: 'authorization header malformed'});
+    }
+    
+    
+    const tokenDecripted = decriptToken(token);
+    const userId = tokenDecripted.userId;
+
+    if(!userId) {
+        return response.status(403).json({ message: 'forbidden' });
+    }
+
     const product = request.body;
-    const createdProduct = await createProductUseCase(product);
+    const createdProduct = await createProductUseCase(product, userId);
 
     return response.status(201).json(createdProduct);
 });
